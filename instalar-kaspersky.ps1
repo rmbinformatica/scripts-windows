88a86aae81a0eb780a5c9f4a26ef5f2bb867aa0f
Renato Monteiro Batista <30217311+renatomb@users.noreply.github.com>
2023-11-20 21:22:05 -0300

    fix broken link

--- instalar-kaspersky.ps1
+++ instalar-kaspersky.ps1
@@ -1,5 +1,5 @@
 cd "C:\instalar"
-# Download do instalador do kaspersky, obtenha o link no site https://doc.rmbinformatica.com.br/ajuda/seguranca/antivirus-kaspersky
+# Download do instalador do kaspersky, obtenha o link no site https://ajuda.rmbinformatica.com.br/seguranca/kaspersky
 Invoke-WebRequest -Uri "https://pdc4.trt.pdc.kaspersky.com/DownloadManagers/ee/eeb8c4d6-ff2c-40f7-9b77-a0c8760651f9/kav21.3.10.391pt_30829.exe" -OutFile kav21.3.10.391pt_30829.exe
 # Download do arquivo de configuracao padrao
 Invoke-WebRequest -Uri "https://rmbinformatica.com.br/pub/kasper.cfg" -OutFile install.cfg
